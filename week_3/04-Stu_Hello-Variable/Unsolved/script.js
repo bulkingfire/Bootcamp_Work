94033536eec210db70f766c962d1f69728773937
bulkingfire <evagelos18@gmail.com>
2022-12-12 21:16:35 -0600

    Monday class

--- week_3/04-Stu_Hello-Variable/Unsolved/script.js
+++ week_3/04-Stu_Hello-Variable/Unsolved/script.js
@@ -1,1 +1,13 @@
 // Write Your JavaScript Code Here
+var personName = "Vageli";
+var pets = 3;
+var funFact = "I am an Engineer"
+
+console.log("Hi my name is " + personName + " and I have " + pets + " pets and a fun fact about me is that " + funFact); 
+
+
+var personName = "Miles";
+var pets = 1;
+var funFact = "I am an football player"
+
+console.log("Hi my name is " + personName + " and I have " + pets + " pets and a fun fact about me is that " + funFact);
--- week_3/06-Stu_Primitive-Types/Unsolved/script.js
+++ week_3/06-Stu_Primitive-Types/Unsolved/script.js
@@ -6,11 +6,17 @@ var five = "Howdy!";
 var six;
 
 // Insert comments to explain what each console log below will log to the console
-console.log(typeof one); 
-console.log(typeof two); 
-console.log(typeof three); 
-console.log(typeof four); 
+//one should log number
+console.log(typeof one);
+//two should log string 
+console.log(typeof two);
+//three should log boolean 
+console.log(typeof three);
+//four should log number; 
+console.log(typeof four);
+//five should log string; 
 console.log(typeof five);
+//six should log undefined 
 console.log(typeof six);
 
 four = "Hello!";
@@ -18,6 +24,9 @@ five = false;
 six = 23;
 
 // Insert comments to explain what each console log below will log to the console
+//string
 console.log(typeof four);
+//boolean
 console.log(typeof five);
+//number 
 console.log(typeof six);
