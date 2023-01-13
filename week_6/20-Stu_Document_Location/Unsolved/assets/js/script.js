98b3aa926ed1f318e4c34e2534950fc1da646083
vageli stathatos <evagelos18@gmail.com>
2023-01-12 20:58:33 -0600

    class

--- week_6/20-Stu_Document_Location/Unsolved/assets/js/script.js
+++ week_6/20-Stu_Document_Location/Unsolved/assets/js/script.js
@@ -3,5 +3,16 @@ var redirectUrl = './404.html';
 
 fetch(badRequestUrl).then(function (response) {
   // Use a conditional to check the response status.
+  console.log(response.status);
+      // Then write the conditional based on that response.status value
+      // Make sure to display the response on the page
+
+      if (response.status !== 200){
+        document.location.href = redirectUrl
+      }
+      return response.json();
+    })
+    
+    
   // If that status equals the conditional, then redirect to the 404 page.
-});
+
